9123ab6c505b81ff1679b6d24158f40d48f101e4
negm <mohamednegm773@gmail.com>
2024-05-14 12:48:52 +0300

    Update userRoutes.js with new endpoint

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -67,16 +67,124 @@ userRouter.get('/dashboard/Classification', checkAuthenticated, (req, res) => {
 // });
 
 // serve (send) the classified image and results to the front end
-userRouter.get('/classification-image', async (req, res) => {
+
+// userRouter.get('/classification-image', (req, res) => {
+//   const imageName = req.params.imageName;
+//   const resultsPath = path.join(__dirname, '../target_images', `${imageName}_results.json`);
+
+//   fs.readFile(resultsPath, 'utf8', (err, data) => {
+//       if (err) {
+//           console.error(`Error reading file from disk: ${err}`);
+//           res.status(500).send('Server error');
+//       } else {
+//           const classificationResults = JSON.parse(data);
+//           const response = {
+//               classificationResults: classificationResults,
+//               imageName: imageName
+//           };
+//           res.json(response);
+//       }
+//   });
+// });
+
+// userRouter.get('/classification-image', async (req, res) => {
+//   const resultsPath = path.join(__dirname, '../target_images/my_custom_results.json');
+
+//   if (fs.existsSync(resultsPath)) {
+//     let classificationResults = JSON.parse(fs.readFileSync(resultsPath, 'utf8'));
+
+//     classificationResults = classificationResults.map((result, index) => ({
+//       ...result,
+//       imageName: `detected_image${index}.jpg` 
+//     }));
+
+//     res.json(classificationResults);
+//   } else {
+//     res.status(404).send('Results not found');
+//   }
+// });
+
+
+// userRouter.get('/classificationimage', async (req, res) => {
+//   const resultsPath = path.join(__dirname, '../target_images/my_custom_results.json');
+//   const imagePath = path.join(__dirname, '../target_images/detected_image.jpg'); 
+
+//   if (fs.existsSync(resultsPath)) {
+//     const classificationResults = JSON.parse(fs.readFileSync(resultsPath, 'utf8'));
+
+//     if (fs.existsSync(imagePath)) {
+//       const response = {
+//         classificationResults: classificationResults,
+//         imageName: 'detected_image.jpg'
+//       };
+
+//       res.json(response);
+//     } else {
+//       res.status(404).send('Detected image not found');
+//     }
+//   } else {
+//     res.status(404).send('Results not found');
+//   }
+// });
+
+
+userRouter.get('/classificationimage', async (req, res) => {
   const resultsPath = path.join(__dirname, '../target_images/my_custom_results.json');
+  const imagePath = path.join(__dirname, '../target_images/detected_image.jpg'); 
 
   if (fs.existsSync(resultsPath)) {
     const classificationResults = JSON.parse(fs.readFileSync(resultsPath, 'utf8'));
-    res.json(classificationResults);
+
+    if (fs.existsSync(imagePath)) {
+      // Read the image file and convert it to base64
+      const imageBuffer = fs.readFileSync(imagePath);
+      const imageBase64 = Buffer.from(imageBuffer).toString('base64');
+
+      const response = {
+        classificationResults: classificationResults,
+        imageName: imageBase64
+      };
+      res.json(response);
+    } else {
+      res.status(404).send('Detected image not found');
+    }
   } else {
     res.status(404).send('Results not found');
   }
 });
+
+
+// userRouter.get('/classification-image', async (req, res) => {
+//   const resultsPath = path.join(__dirname, '../target_images/my_custom_results.json');
+//   const imageName = 'detected_image.jpg'; 
+
+//   if (fs.existsSync(resultsPath)) {
+//     const classificationResults = JSON.parse(fs.readFileSync(resultsPath, 'utf8'));
+    
+//       //Include the image name in the response
+//     const response = {
+//       classificationResults: classificationResults,
+//       imageName: imageName 
+//     };
+
+//     res.json(response);
+//   } else {
+//     res.status(404).send('Results not found');
+//   }
+// });
+
+
+
+// userRouter.get('/classification-image', async (req, res) => {
+//   const resultsPath = path.join(__dirname, '../target_images/my_custom_results.json');
+
+//   if (fs.existsSync(resultsPath)) {
+//     const classificationResults = JSON.parse(fs.readFileSync(resultsPath, 'utf8'));
+//     res.json(classificationResults);
+//   } else {
+//     res.status(404).send('Results not found');
+//   }
+// });
 
 // Logout route
 userRouter.delete("/logout", (req, res) => {
